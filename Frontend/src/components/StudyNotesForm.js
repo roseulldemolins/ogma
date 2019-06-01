094748d01f6c90b150166a41400e886aa5d6f5b5
Jim <a9372275@9372275s-MacBook-Pro.local>
2019-06-01 18:21:37 +0100

    txt area & sub button display on study notes form

--- Frontend/src/components/StudyNotesForm.js
+++ Frontend/src/components/StudyNotesForm.js
@@ -5,17 +5,35 @@ class StudyNotesForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      placeholder: null
+      inputtedText: ""
     }
-    // bind funcs here
+    this.handleFormSubmit = this.handleFormSubmit.bind(this);
+    this.handleFormSubmit = this.handleFormSubmit.bind(this);
+  }
+
+  handleFormSubmit(evt) {
+    evt.preventDefault();
+    const newStudyNote = {
+      study_note_text: this.state.inputtedText,
+      type: "study_note",
+      topic: "coding" // TODO: hard coded topic needs updated when multi-topics are introduced
+    }
+    this.props.addNewStudyNote(newStudyNote);
+  }
+
+  handleTextChange() {
+
   }
 
   render() {
     return (
       <div id='study-notes-form-container'>
         <h1>Ogma Study Notes</h1>
-
-        
+        <form onSubmit={this.handleFormSubmit} autoComplete='off'>
+          <textarea onChange={this.handleTextChange} rows="4" cols="50">Type a new study note here.</textarea>
+          <input type='submit' value='Submit Note' />
+          <h4 id="list-divider"className="ui horizontal divider header">Study Notes</h4>
+        </form>
       </div>
     )
   }
