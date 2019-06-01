df8c1580e33eb8f1abedcb7bcf75db55bf1f3518
Jim <a9372275@9372275s-MacBook-Pro.local>
2019-06-01 21:17:43 +0100

    dispatch of new note working

--- Frontend/src/reducers/questions_reducer.js
+++ Frontend/src/reducers/questions_reducer.js
@@ -2,6 +2,8 @@ const questionsReducer = (state = [], action) => {
   switch (action.type) {
     case 'LOAD_QUESTIONS_DATA':
       return [...state, ...action.questionsData]
+    case 'ADD_NEW_STUDY_NOTE':
+      return [...state, action.newStudyNote]
     default:
       return state
   }
