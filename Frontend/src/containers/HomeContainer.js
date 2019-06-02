6c9990a2c23e795fb699f6f16af2791789b3c89b
9338190 <a9338190@9338190s-MacBook-Pro.local>
2019-06-02 21:48:08 +0100

    changed home page header title and added paragraph

--- Frontend/src/containers/HomeContainer.js
+++ Frontend/src/containers/HomeContainer.js
@@ -4,7 +4,8 @@ import './HomeContainer.css';
 const HomeContainer = () => {
   return(
     <div id='home-page'>
-    <h1 id='ogma-home'>Ogma</h1>
+    <h1 id='ogma-home-header'>Ogma learning</h1>
+    <p id='ogma-home-paragraph'>Only sky is the limit</p>
     </div>
   )
 }
