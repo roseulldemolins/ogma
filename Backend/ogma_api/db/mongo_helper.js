5d53c83ff64920ef537245c157ff05ec4c422b60
Rose Ulldemolins <a9854261@9854261s-MacBook-Pro.local>
2019-06-03 09:25:03 +0100

    added routes for database

--- Backend/ogma_api/db/mongo_helper.js
+++ Backend/ogma_api/db/mongo_helper.js
@@ -14,53 +14,53 @@ class MongoHelper {
       return collection.find().toArray();
     });
   }
-//
-//   static create(coll, payload) {
-//     // Connect using the connection string
-//     return MongoClient.connect(
-//       HOST,
-//       { useNewUrlParser: true }
-//     ).then(client => {
-//       const collection = client.db(DB_NAME).collection(coll);
-//       return collection.insertOne(payload);
-//     });
-//   }
-//
-//   static delete(coll, id) {
-//     // Connect using the connection string
-//     return MongoClient.connect(
-//       HOST,
-//       { useNewUrlParser: true }
-//     ).then(client => {
-//       const collection = client.db(DB_NAME).collection(coll);
-//       return collection.deleteOne({ _id: ObjectID(id) });
-//     });
-//   }
-//
-//   static update(coll, id, payload) {
-//     // Connect using the connection string
-//     return MongoClient.connect(
-//       HOST,
-//       { useNewUrlParser: true }
-//     ).then(client => {
-//       const collection = client.db(DB_NAME).collection(coll);
-//       return collection.updateOne({ _id: ObjectID(id) }, { $set: payload });
-//     });
-//   }
-//
-//   static addComment(coll, id, payload) {
-//     // Connect using the connection string
-//     return MongoClient.connect(
-//       HOST,
-//       { useNewUrlParser: true }
-//     ).then(client => {
-//       const collection = client.db(DB_NAME).collection(coll);
-//       return collection.updateOne(
-//         { _id: ObjectID(id) },
-//         { $push: { comments: payload } }
-//       );
-//     });
-//   }
+
+  static create(coll, payload) {
+    // adding a question/topic to the database
+    return MongoClient.connect(
+      HOST,
+      { useNewUrlParser: true }
+    ).then(client => {
+      const collection = client.db(DB_NAME).collection(coll);
+      return collection.insertOne(payload);
+    });
+  }
+
+  static delete(coll, id) {
+    // removing a question/topic from the database
+    return MongoClient.connect(
+      HOST,
+      { useNewUrlParser: true }
+    ).then(client => {
+      const collection = client.db(DB_NAME).collection(coll);
+      return collection.deleteOne({ _id: ObjectID(id) });
+    });
+  }
+
+  static update(coll, id, payload) {
+    // Updating a question/topic using the id
+    return MongoClient.connect(
+      HOST,
+      { useNewUrlParser: true }
+    ).then(client => {
+      const collection = client.db(DB_NAME).collection(coll);
+      return collection.updateOne({ _id: ObjectID(id) }, { $set: payload });
+    });
+  }
+  // 
+  // static addComment(coll, id, payload) {
+  //   // Connect using the connection string
+  //   return MongoClient.connect(
+  //     HOST,
+  //     { useNewUrlParser: true }
+  //   ).then(client => {
+  //     const collection = client.db(DB_NAME).collection(coll);
+  //     return collection.updateOne(
+  //       { _id: ObjectID(id) },
+  //       { $push: { comments: payload } }
+  //     );
+  //   });
+  // }
 }
 
 module.exports = MongoHelper;
--- Backend/ogma_api/routes/questions.js
+++ Backend/ogma_api/routes/questions.js
@@ -9,27 +9,27 @@ router.get("/", function(req, res) {
   });
 });
 
-// /* Create a new posts */
-// router.post("/", function(req, res) {
-//   MongoHelper.create("posts", req.body).then(results => {
-//     res.status(201).json("New post created");
-//   });
-// });
-//
-// /* Update a posts */
-// router.put("/:id", function(req, res) {
-//   MongoHelper.update("posts", req.params.id, req.body).then(results => {
-//     res.status(200).json("Post updated");
-//   });
-// });
-//
-// /* Delete a posts */
-// router.delete("/:id", function(req, res) {
-//   MongoHelper.delete("posts", req.params.id).then(results => {
-//     res.status(204).json("Post updated");
-//   });
-// });
-//
+/* Create a new question/topic */
+router.post("/", function(req, res) {
+  MongoHelper.create("questions", req.body).then(results => {
+    res.status(201).json("New question created");
+  });
+});
+
+/* Update a question */
+router.put("/:id", function(req, res) {
+  MongoHelper.update("questions", req.params.id, req.body).then(results => {
+    res.status(200).json("Question updated");
+  });
+});
+
+/* Delete a question */
+router.delete("/:id", function(req, res) {
+  MongoHelper.delete("questions", req.params.id).then(results => {
+    res.status(204).json("Question removed");
+  });
+});
+
 // /* Add new comment */
 // router.post("/:post_id/comments", function(req, res) {
 //   MongoHelper.addComment("posts", req.params.post_id, req.body).then(
