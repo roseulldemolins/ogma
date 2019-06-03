72d6eb3dad2f335f74c0d37f1fd0e437751ef1f6
Rose Ulldemolins <a9854261@9854261s-mbp.home>
2019-06-03 06:52:59 +0100

    css changes to pairs cards

---
+++ Frontend/src/components/PairsCard.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactCardFlip from "react-card-flip";
+import { connect } from 'react-redux';
+
+const PairsCard = (props) => {
+
+  console.log(props);
+
+return(
+  <ReactCardFlip isFlipped={props.isFlipped} flipSpeedBackToFront={1} flipSpeedFrontToBack={1} >
+    <button id={props.id} className={`card card-front ${props.cardNumber !== -1 ? "" : "hide-card"}`} onClick={props.handleClick} key="front">
+
+    </button>
+
+    <button id={props.id} className={`card card-back ${props.cardNumber !== -1 ? "" : "hide-card"}`} onClick={props.handleClick} key="back">
+      { props.cardNumber }
+    </button>
+  </ReactCardFlip>
+)
+}
+
+
+export default PairsCard
---
+++ Frontend/src/components/PairsHeader.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const PairsHeader = ({ restartGame }) => (
+  <div className="grid-header-container">
+    <div className="justify-left timer"></div>
+    <div className="justify-center game-status-text"></div>
+    <div className="justify-end">
+      <button onClick={restartGame} className="black ui button">Restart Game</button>
+    </div>
+  </div>
+);
+
+export default PairsHeader;
