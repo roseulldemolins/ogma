fd22c5974b8d03b4a12edbbd41f2112ee794acfd
Rose Ulldemolins <a9854261@9854261s-MacBook-Pro.local>
2019-06-03 09:37:50 +0100

    css changes

--- Frontend/src/reducers/questions_reducer.js
+++ Frontend/src/reducers/questions_reducer.js
@@ -2,6 +2,8 @@ const questionsReducer = (state = [], action) => {
   switch (action.type) {
     case 'LOAD_QUESTIONS_DATA':
       return [...state, ...action.questionsData]
+    case 'ADD_QUESTION':
+      return [action.question, ...state]
     default:
       return state
   }
