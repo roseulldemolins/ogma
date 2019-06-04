4e648cdd902f62e13314c399a5ae8eb6863bdc2b
9338190 <a9338190@9338190s-MacBook-Pro.local>
2019-06-04 14:23:45 +0100

    started fixing bug #11

--- Frontend/src/reducers/questions_reducer.js
+++ Frontend/src/reducers/questions_reducer.js
@@ -9,6 +9,8 @@ const questionsReducer = (state = [], action) => {
       case 'DELETE_NOTE':
       console.log('action.index in the reducer: ', action.id);
       return [...state.slice(0, action.id), ...state.slice(action.id + 1)]
+      // const newState = [];
+      // state.forEach()
     default:
       return state
   }
