5b880c52ddd7e61ff06736145beab3ee97c76bf5
Jim <a9372275@9372275s-MacBook-Pro.local>
2019-06-04 17:06:57 +0100

    updated db seeds & bug list

--- Backend/ogma_api/db/ogma_db_seeds.js
+++ Backend/ogma_api/db/ogma_db_seeds.js
@@ -5,27 +5,27 @@ db.dropDatabase();
 db.questions.insertMany([
 
 {
-  study_note_text:"Note to self - add more study notes to test db",
+  study_note_text:"Note to self - add more study notes",
   type:"study_note",
   topic:"coding"
 },
 {
-  study_note_text:"Test Study Note - rhubarb, rhubarb",
+  study_note_text:"Remember - PDA test on Monday!",
   type:"study_note",
   topic:"coding"
 },
 {
-  study_note_text:"Test Study Note - blah, blah, blah",
+  study_note_text:"Design documents",
   type:"study_note",
   topic:"coding"
 },
 {
-  study_note_text:"Note to self - should this really be a big <ul>?",
+  study_note_text:"Note to self - should study notes really be a big <ul>?",
   type:"study_note",
   topic:"coding"
 },
 {
-  study_note_text:"Note to self - do these study notes need any other fields?",
+  study_note_text:"Note to self - do study notes need any other fields?",
   type:"study_note",
   topic:"coding"
 },
@@ -93,7 +93,7 @@ db.questions.insertMany([
   topic:"coding"
 },
 {
-  question_text:"Inside which HTML tag can Javascript be written?",
+  question_text:"Inside which HTML tag can JavaScript be written?",
   answer_text:"<script>",
   sub_topic:"HTML",
   type:"q_and_a",
@@ -114,9 +114,101 @@ db.questions.insertMany([
   topic:"coding"
 },
 {
+  question_text:"True or false: is <h1>Text</h1> the right way to make a header in HTML?",
+  answer_text:"True",
+  sub_topic:"HTML",
+  type:"q_and_a",
+  topic:"coding"
+},
+{
+  question_text:"What does SQL stand for?",
+  answer_text:"Structured Query Language",
+  sub_topic:"Database",
+  type:"q_and_a",
+  topic:"coding"
+},
+{
+  question_text:"Which SQL statement is used to extract data from a database?",
+  answer_text:"SELECT",
+  sub_topic:"Database",
+  type:"q_and_a",
+  topic:"coding"
+},
+{
+  question_text:"What's wrong with the following JS: if myVar = "x" ...?",
+  answer_text:"A single = is an assignment, not a comparison",
+  sub_topic:"Javascript",
+  type:"q_and_a",
+  topic:"coding"
+},
+{
+  question_text:"What's the difference between == and ===?",
+  answer_text:"Strict equality (===) requires matching data types, whereas loose equality (==) performs type conversion before comparing",
+  sub_topic:"Javascript",
+  type:"q_and_a",
+  topic:"coding"
+},
+{
+  question_text:"How can you add a comment in a JavaScript?",
+  answer_text:"// This is a JavaScript comment",
+  sub_topic:"Javascript",
+  type:"q_and_a",
+  topic:"coding"
+},
+{
+  question_text:"True or false: JavaScript is another name for Java",
+  answer_text:"False",
+  sub_topic:"Javascript",
+  type:"q_and_a",
+  topic:"coding"
+},
+{
+  question_text:"Is JavaScript case-sensitive?",
+  answer_text:"Yes",
+  sub_topic:"Javascript",
+  type:"q_and_a",
+  topic:"coding"
+},
+{
+  question_text:"How do you daisychain terminal commands?",
+  answer_text:"Use a pipe (|) to join unix commands",
+  sub_topic:"Unix",
+  type:"q_and_a",
+  topic:"coding"
+},
+{
+  question_text:"What is encapsulation?",
+  answer_text:"A way of restricting direct access to an object's properties",
+  sub_topic:"Programming",
+  type:"q_and_a",
+  topic:"coding"
+},
+{
+  question_text:"What is inheritance?",
+  answer_text:"A way of basing an object or class on another object or class, whereby the child retains properties of the parent",
+  sub_topic:"Programming",
+  type:"q_and_a",
+  topic:"coding"
+},
+{
+  question_text:"What is MongoDB's approximate equivalent of a relational database's table?",
+  answer_text:"A collection",
+  sub_topic:"Database",
+  type:"q_and_a",
+  topic:"coding"
+},
+{
+  question_text:"Which MongoDB method would you use to add an entry to the database?",
+  answer_text:"insertOne()",
+  sub_topic:"Database",
+  type:"q_and_a",
+  topic:"coding"
+},
+
+{
   question_text:"What is the decimal equivalent of OxFF?",
   answer_text:"255",
-  sub_topic:"Javascript",
+  sub_topic:"Programming",
   type:"q_and_a",
   topic:"coding"
 }
