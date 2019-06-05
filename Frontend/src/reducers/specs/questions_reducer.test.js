1e378a26762ced51561be736d19209cd33575243
9338190 <a9338190@9338190s-MacBook-Pro.local>
2019-06-05 13:45:16 +0100

    testing reducer

--- Frontend/src/reducers/specs/questions_reducer.test.js
+++ Frontend/src/reducers/specs/questions_reducer.test.js
@@ -8,18 +8,43 @@ import questionsReducer from '../questions_reducer.js'
 
 describe('Reducer testing', () => {
   beforeEach(() => {
-
+    const questionData = [
+      {
+        question_text:"What does SQL stand for?",
+        answer_text:"Structured Query Language",
+        sub_topic:"Database",
+        type:"q_and_a",
+        topic:"coding"
+      },
+      {
+        question_text:"What does CSS stand for?",
+        answer_text:"Cascading Style Sheets",
+        sub_topic:"CSS",
+        type:"q_and_a",
+        topic:"coding"
+      },
+      {
+        question_text:"What are JavaScript Data Types?",
+        answer_text:"Number, string, boolean, object and undefined",
+        sub_topic:"Javascript",
+        type:"q_and_a",
+        topic:"coding"
+      },
+      ]
   })
   it('should return the initial state', () => {
     expect(questionsReducer(undefined, {})).toEqual([])
   });
 
-  xit('should handle LOAD_QUESTIONS_DATA', () => {
-    const startFetch = {
-      type: action.LOAD_QUESTIONS_DATA
+  it('should handle LOAD_QUESTIONS_DATA', () => {
+    const action = {
+      type: 'LOAD_QUESTIONS_DATA',
+      questionsData:questionData
     };
 
-      expect(questionsReducer({}, startFetch)).toEqual({})
+    const initialState = []
+
+      expect(questionsReducer(initialState, questionData)).toEqual([], questionData)
 
   });
 
