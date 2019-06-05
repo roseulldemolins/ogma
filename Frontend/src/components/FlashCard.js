408ee05795638671c10163deac9c1e37c98dcfe1
Rose Ulldemolins <a9854261@9854261s-MacBook-Pro.local>
2019-06-05 12:13:36 +0100

    working modal for flashcards

--- Frontend/src/components/FlashCard.js
+++ Frontend/src/components/FlashCard.js
@@ -41,20 +41,35 @@ class FlashCard extends React.Component {
     const index = this.state.randomIndex || this.props.randomIndex
       return (
         <div id='flash-card-container'>
-        <div className="flipcard">
-        <ReactCardFlip isFlipped={this.state.isFlipped} flipDirection="vertical" flipSpeedBackToFront={0.20} flipSpeedFrontToBack={0.20}>
-          <div id='front' key="front" value={index} onClick={this.handleFlipClick}>
-            <p className='subtopic'>{this.props.questionList[index].sub_topic}</p>
-            <p className='text'>{this.props.questionList[index].question_text}</p>
-          </div>
-
-          <div id='back' key="back" value={index} onClick={this.handleFlipClick}>
-            <p className='subtopic'>{this.props.questionList[index].sub_topic}</p>
-            <p className='text'>{this.props.questionList[index].answer_text}</p>
-          </div>
-        </ReactCardFlip>
-        <button id='black-ui-button' className="black ui button" onClick={this.handleNewCardClick}>New card</button>
+          <div className="flipcard">
+          <ReactCardFlip isFlipped={this.state.isFlipped} flipDirection="vertical" flipSpeedBackToFront={0.20} flipSpeedFrontToBack={0.20}>
+            <div id='front' key="front" value={index} onClick={this.handleFlipClick}>
+              <p className='subtopic'>{this.props.questionList[index].sub_topic}</p>
+              <p className='text'>{this.props.questionList[index].question_text}</p>
+            </div>
+            <div id='back' key="back" value={index} onClick={this.handleFlipClick}>
+              <p className='subtopic'>{this.props.questionList[index].sub_topic}</p>
+              <p className='text'>{this.props.questionList[index].answer_text}</p>
+            </div>
+          </ReactCardFlip>
         </div>
+        <button onClick={this.handleNewCardClick}>New card</button>
+        <button type="button" data-toggle="modal" data-target="#flashcardsModal">Instructions</button>
+        <div className="modal fade" id="flashcardsModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div className="modal-dialog modal-dialog-centered" role="document">
+            <div className="modal-content">
+              <div className="modal-header">
+              <h5 className="modal-title" id="glashcardsModalLabel">How to play</h5>
+              <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div className="modal-body">
+        Here you can view all of your questions from the bank on index cards. First, read the question on the front of the card and try to answer it. Once you think you have the answer you can click on the index card to flip it and reveal the answer, simply click the card again if you want to see the question. If you're confident you know it you can click 'New Card' to try and get the next one.
+      </div>
+    </div>
+  </div>
+</div>
         </div>
       )
   }
