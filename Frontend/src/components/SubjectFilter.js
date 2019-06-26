56b2b7e1dce1570c2f91981626563a210f62ceaf
bsharp807 <a0030619@0030619s-MacBook-Pro.local>
2019-06-26 15:57:18 +0100

    filtered array created for dropdown

--- Frontend/src/components/SubjectFilter.js
+++ Frontend/src/components/SubjectFilter.js
@@ -12,8 +12,17 @@ class SubjectFilter extends Component {
   }
 
   filterTopics(array){
-    console.log(array);
-    return array
+    const newArray = array.map((element) => {
+      return element.sub_topic
+    })
+    const removedUndefined = newArray.filter((element) => {
+      return element
+    })
+    const filteredArray = removedUndefined.filter((element, index, array) => {
+        return array.findIndex(i => i === element) === index;
+      });
+    const sortedArray = filteredArray.sort()
+    return sortedArray
   }
 
   componentDidUpdate(prevProps) {
