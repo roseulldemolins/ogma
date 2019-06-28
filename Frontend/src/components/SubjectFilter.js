c6f9f7f778939c1c50d0a372215056d2e6a6d980
bsharp807 <a0030619@0030619s-MacBook-Pro.local>
2019-06-28 13:59:02 +0100

    refactored filtering system to use helper file

--- Frontend/src/components/SubjectFilter.js
+++ Frontend/src/components/SubjectFilter.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import FilterList from './FilterList.js';
 
+const filteringMechanism = require('../helpers/FilteringMechanism.js');
+
 class SubjectFilter extends Component {
   constructor(props){
     super(props)
@@ -13,17 +15,7 @@ class SubjectFilter extends Component {
   }
 
   filterTopics(array){
-    const newArray = array.map((element) => {
-      return element.sub_topic
-    })
-    const removedUndefined = newArray.filter((element) => {
-      return element
-    })
-    const filteredArray = removedUndefined.filter((element, index, array) => {
-        return array.findIndex(i => i === element) === index;
-      });
-    const sortedArray = filteredArray.sort()
-    return sortedArray
+    return filteringMechanism(array)
   }
 
   componentDidUpdate(prevProps) {
