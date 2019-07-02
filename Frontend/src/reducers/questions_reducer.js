ea89f45394e1efcfe702b8f03e103124b31ee8c0
bsharp807 <a0030619@0030619s-MacBook-Pro.local>
2019-07-02 16:19:19 +0100

    new additions to subtopics now keep preferences the same

--- Frontend/src/reducers/questions_reducer.js
+++ Frontend/src/reducers/questions_reducer.js
@@ -1,10 +1,15 @@
+const criteria = require('../helpers/Criteria.js');
+
 const questionsReducer = (state = {questions: [], options: []}, action) => {
   switch (action.type) {
     case 'LOAD_QUESTIONS_DATA':
       const questionLoad = {...state, questions: [...action.questionsData]}
       return questionLoad
     case 'UPDATE_FILTER':
-      return {...state, options: [...action.filterArray]}
+      if(state.options.length){
+        const newFilter = criteria(state.options, action.filterArray);
+        return {...state, options: [...newFilter]}
+      } return {...state, options: [...action.filterArray]}
     default:
       return state
   }
