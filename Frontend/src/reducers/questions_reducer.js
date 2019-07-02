8eb4c4dc89573a994b946176d34ef8e730b71d89
bsharp807 <a0030619@0030619s-MacBook-Pro.local>
2019-07-02 09:35:36 +0100

    cleaned reducer

--- Frontend/src/reducers/questions_reducer.js
+++ Frontend/src/reducers/questions_reducer.js
@@ -3,20 +3,8 @@ const questionsReducer = (state = {questions: [], options: []}, action) => {
     case 'LOAD_QUESTIONS_DATA':
       const questionLoad = {...state, questions: [...action.questionsData]}
       return questionLoad
-    case 'ADD_QUESTION':
-      return {...state, questions: [action.newQuestion, ...state.questions]}
-    case 'ADD_NEW_STUDY_NOTE':
-      return {...state, questions: [...state.questions, action.newStudyNote]}
     case 'UPDATE_FILTER':
       return {...state, options: [...action.filterArray]}
-    case 'DELETE_NOTE':
-      const newState = {...state, questions: []};
-      state.questions.forEach((note) => {
-        if(note._id !== action.id){
-          newState.questions.push(note)
-        }
-      })
-      return newState;
     default:
       return state
   }
