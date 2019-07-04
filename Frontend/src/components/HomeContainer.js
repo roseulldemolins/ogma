2c5d31992fc1254d9c18c04b472588064909e4e8
bsharp807 <a0030619@0030619s-MacBook-Pro.local>
2019-07-04 10:00:32 +0100

    added hidden easter egg ;) shrek is love shrek is life

--- Frontend/src/components/HomeContainer.js
+++ Frontend/src/components/HomeContainer.js
@@ -4,8 +4,8 @@ import './HomeContainer.css';
 const HomeContainer = () => {
   return(
     <div id='home-page'>
-    <h1 id='ogma-home-header'>Ogma learning</h1>
-    <p id='ogma-home-paragraph'>Only sky is the limit</p>
+    <h1 id='ogma-home-header'>O<a href="https://youtu.be/engvN0hY8Bo?t=5" rel="noopener noreferrer" target="_blank">g</a>ma learning</h1>
+    <p id='ogma-home-paragraph'>The sky is the limit</p>
     </div>
   )
 }
